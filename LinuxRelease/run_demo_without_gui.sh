8c723b3943e2fd334cd5b8c491203767bf6b71ff
iamywang <41533488+iamywang@users.noreply.github.com>
2023-03-14 14:25:26 +0800

    run shell

--- LinuxRelease/run_demo_without_gui.sh
+++ LinuxRelease/run_demo_without_gui.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd "$( dirname "$0" )"
-./Robot -f /run/media/iamywang/Data/workspace/CodeCraft-2023/build/main -m maps/1.txt
-# ./Robot -f /run/media/iamywang/Data/workspace/CodeCraft-2023/build/main -m maps/2.txt
-# ./Robot -f /run/media/iamywang/Data/workspace/CodeCraft-2023/build/main -m maps/3.txt
-# ./Robot -f /run/media/iamywang/Data/workspace/CodeCraft-2023/build/main -m maps/4.txt
+v1=$(./Robot -f -l ERR /run/media/iamywang/Data/workspace/CodeCraft-2023/build/main -m maps/1.txt | jq -r '.score')
+v2=$(./Robot -f -l ERR /run/media/iamywang/Data/workspace/CodeCraft-2023/build/main -m maps/2.txt | jq -r '.score')
+v3=$(./Robot -f -l ERR /run/media/iamywang/Data/workspace/CodeCraft-2023/build/main -m maps/3.txt | jq -r '.score')
+v4=$(./Robot -f -l ERR /run/media/iamywang/Data/workspace/CodeCraft-2023/build/main -m maps/4.txt | jq -r '.score')
+echo $v1+$v2+$v3+$v4 | bc
